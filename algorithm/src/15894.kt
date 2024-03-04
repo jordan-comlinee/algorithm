5eae05ef5b190149bcf9148984e126ec00fcbf85
Jordan <leesoyoung0112@gmail.com>
2024-03-04 21:29:03 +0900

    20240304

---
+++ algorithm/src/15894.kt
@@ -0,0 +1,3 @@
+fun main() {
+    print("${readLine()!!.toUInt() * 4.toUInt()}")
+}
---
+++ algorithm/src/27323.kt
@@ -0,0 +1,9 @@
+import java.util.Scanner
+
+fun main() = with(Scanner(System.`in`)) {
+    var a = nextInt()
+    var b = nextInt()
+
+    print("${a*b}")
+
+}
