97f64ae8c3c0add4f86cb317eedba1f8cd8d9779
Jordan <leesoyoung0112@gmail.com>
2024-03-26 22:02:31 +0900

    20240326

--- algorithm/src/19532.kt
+++ algorithm/src/19532.kt
@@ -1,21 +1,19 @@
 import java.util.Scanner
-// ax + by = c 에서 x와 y를 구하는 방법??
-// x와 y를 만족하도록 하려면??
-// ax + by = c (x = (c/a) - (b/a)y)
-// dx + ey = f d((c/a)- (b/a)y + ey = f)
-// (dc/a) - (bd/a)y + ey = f
-// (e-(bd/a))y = f - (dc/a)
-// y = (f - (dc/a)) / (e - (bd/a))
-
-
 
 fun main() = with(Scanner(System.`in`)) {
-    var homework = mutableListOf<Int>()
-    for (i in 0..5) {
-        homework.add(nextInt())
-    }
-    var y = (homework[5]-(homework[3]*homework[2]/homework[0])) / (homework[4] - (homework[1]*homework[3]/homework[0]))
-    var x = (homework[2]-homework[1]*y)/homework[0]
-    println("$x $y")
+    val a = nextInt()
+    val b = nextInt()
+    val c = nextInt()
+    val d = nextInt()
+    val e = nextInt()
+    val f = nextInt()
 
+    for (i in -999 until 1000) {
+        for (j in -999 until 1000) {
+            if (a * i + b * j == c && d * i + e * j == f) {
+                println("$i $j")
+                break
+            }
+        }
+    }
 }
