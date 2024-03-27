5be2aa743301d5549b9472f0ede064e8ad5c8efe
Jordan <leesoyoung0112@gmail.com>
2024-03-27 16:19:53 +0900

    20240327

---
+++ algorithm/src/2839.kt
@@ -0,0 +1,13 @@
+import java.util.Scanner
+
+fun main() = with(Scanner(System.`in`)) {
+    val n = nextInt()
+    var count = -1
+    for (i in (n/5) downTo 0){
+        if ( (n-i*5)%3==0 ) {
+            count = ((n-i*5)/3) + i
+            break
+        }
+    }
+    println(count)
+}
