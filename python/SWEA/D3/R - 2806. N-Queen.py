9dfa1e1d61d89ec362cdcd4821abd18c8bc56af4
Jordan <leesoyoung0112@gmail.com>
2024-05-16 17:30:35 +0900

    2024-05-16 D3 N-Queen

---
+++ python/SWEA/D3/R - 2806. N-Queen.py
@@ -0,0 +1,28 @@
+def dfs(depth):
+    global result
+    if depth == N :
+        result += 1
+        return
+    for i in range(N) :
+        if visited[i] == False :
+            board[depth] = i
+            if check(depth) :
+                visited[i] = True
+                dfs(depth+1)
+                visited[i] = False
+def check(n):
+    for i in range(n):
+        if (board[n] == board[i] ) or (n-i == abs(board[n] - board[i])) :
+            return False
+    return True
+
+
+T = int(input())
+
+for t in range(1, T+1) :
+    N = int(input())
+    board = [0]*N
+    visited = [False]*N
+    result = 0
+    dfs(0)
+    print(f"#{t} {result}")
