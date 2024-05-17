45675e14e28f7ee0bb4ff7438965c3699e0d329c
Jordan <leesoyoung0112@gmail.com>
2024-05-17 21:18:28 +0900

    2024-05-17 D2 달팽이 숫자

--- python/SWEA/D2/1954. 달팽이 숫자.py
+++ python/SWEA/D2/1954. 달팽이 숫자.py
@@ -1,18 +1,25 @@
-def fill_snail(snail):
-    fill_list = list(range(1, len(snail)+1))
-    fill_list.extend(range(1, len(snail)))
-    fill_list.sort(reverse=True)
-    move_list = [[1, 0], [0, -1], [-1, 0], [0, 1]]
-    while(len(fill_list) > 0) :
-        dx, dy = 0
-    #for i in range(len(snail)):
+def make_snail(N) :
+    global snail
+    x, y, d = 0, 0, 4
+    move = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+    for i in range(1, N*N+1) :
+        snail[x][y] = i
+        if (0 <= x+move[d%4][0] < N and 0 <= y+move[d%4][1] < N and snail[x+move[d%4][0]][y+move[d%4][1]] == 0) :
+            x += move[d%4][0]
+            y += move[d%4][1]
+        else:
+            d += 1
+            x += move[d%4][0]
+            y += move[d%4][1]
 
-    return snail
+T = int(input())
 
-t = int(input())
-
-for _ in range(1, t+1):
-    n = int(input())
-    snail = [[0]*n]*n
-    snail = fill_snail(snail)
-    print(f"#{_}")
+for t in range(1, T+1) :
+    N = int(input())
+    snail = [[0]*N for _ in range(N)]
+    make_snail(N)
+    print(f"#{t}")
+    for i in range(N):
+        for j in range(N):
+            print(f"{snail[i][j]}", end=" ")
+        print()
