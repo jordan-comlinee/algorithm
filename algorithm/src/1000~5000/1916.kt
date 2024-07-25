24818f748db498543311e3de51d7df208ea744e9
Jordan <leesoyoung0112@gmail.com>
2024-07-25 14:18:43 +0900

    2024-07-25 1916. 최소비용 구하기: 복습 유의

--- algorithm/src/1000~5000/1916.kt
+++ algorithm/src/1000~5000/1916.kt
@@ -1,3 +1,5 @@
+// 나무위키를 통해 다익스트라 알고리즘 이해 필요
+// https://namu.wiki/w/%EB%8B%A4%EC%9D%B5%EC%8A%A4%ED%8A%B8%EB%9D%BC%20%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98
 package `1000~5000`
 import java.io.*
 import java.util.*
@@ -8,7 +10,7 @@ private lateinit var dist : IntArray
 private const val INF = Int.MAX_VALUE/4
 private var N = 0
 private var M = 0
-
+// 노드 구하기
 private class Node(var nodeNum : Int, var weight : Int) : Comparable<Node> {
     override fun compareTo(other : Node) : Int {
         return weight - other.weight
@@ -19,8 +21,8 @@ fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     N = br.readLine().toInt()
     M = br.readLine().toInt()
-
-    dist = IntArray(N+1) {INF}
+    // dist : 목적지까지 계산된 최단 거리
+    dist = IntArray(N+1) {INF}  // 아직 방문하지 않은 거리는 초기값이 무한(Int의 최대값)
     list = ArrayList()
     for (i in 0..N) {
         list.add(ArrayList())
