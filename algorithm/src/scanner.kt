d2ad46511ffe49aff92088138fdb18b86e7c7cf5
Jordan <leesoyoung0112@gmail.com>
2024-07-26 16:58:58 +0900

    2024-07-26 1654 review

--- algorithm/src/scanner.kt
+++ algorithm/src/scanner.kt
@@ -1,11 +1,29 @@
 import java.util.*
+import java.io.*
+import kotlin.math.*
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val (K, N)  = br.readLine().split(" ").map{ it.toInt() }
+    val cable = LongArray(N)
+    repeat(K) {k ->
+        cable[k] = br.readLine().toLong()
+    }
+    var end : Long = cable.max() + 1
+    var start : Long = 1
+    while ( start+1 < end ) {
+        var n : Long = 0
+        val mid = (end + start)/2
+        repeat(K) { k->
+            n += (cable[k]) / mid
 
-fun main(args: Array<String>) {
-    // 여러 변수를 입력받아야 하는 경우 (Int)
-    //var inputInt = Scanner(System.`in`)
-    //println("${inputInt.nextInt()+inputInt.nextInt()}")
-
-    var input = Scanner(System.`in`)
-    println("${input.next()}")
+        }
+        if (n >= N) {
+            start = mid
+        }
+        else {
+            end = mid
+        }
+    }
+    println(start)
 
 }
