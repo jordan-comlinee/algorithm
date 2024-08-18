54487f99b7359e34d908d6e84f11a8348cf32814
Jordan <leesoyoung0112@gmail.com>
2024-08-18 17:11:29 +0900

    2024-08-18 15650 reviewed

--- algorithm/src/15001~20000/15650.kt
+++ algorithm/src/15001~20000/15650.kt
@@ -10,31 +10,32 @@ private var N = 0
 private var M = 0
 private lateinit var visited : BooleanArray
 private lateinit var n : IntArray
-
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
     val st = StringTokenizer(br.readLine())
     N = st.nextToken().toInt()
     M = st.nextToken().toInt()
-    n = IntArray(N+1) { i->i }
     visited = BooleanArray(N+1) {false}
+    n = IntArray(N+1) {i->i}
     solution(1, 0, "", bw)
     bw.flush()
     bw.close()
     br.close()
 }
 
-fun solution(idx : Int, len : Int, str : String ,bw : BufferedWriter) {
+fun solution(idx : Int, len : Int, str : String, bw : BufferedWriter) {
     if (len == M) {
-        bw.write(str)
-        bw.write("\n")
+        bw.write("$str \n")
     }
-
-    for (i in idx..N) {
+    // 첫번째 수 고르기
+    for (i in idx .. N) {
+        // 방문하지 않았다면 망문 처리 하기
         if (!visited[i]) {
             visited[i] = true
+            // 그 다음 방문 처리 점화식으로 작성하기
             if (len == 0) {
+                // 처음에 띄어쓰기 있으면 X
                 solution(i, 1, n[i].toString(), bw)
             }
             else {
@@ -43,5 +44,4 @@ fun solution(idx : Int, len : Int, str : String ,bw : BufferedWriter) {
             visited[i] = false
         }
     }
-
 }
