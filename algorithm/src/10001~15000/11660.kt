cb98c116d34f5910ef21ab5d132bb73d22673c43
Jordan <leesoyoung0112@gmail.com>
2024-10-13 21:59:20 +0900

    2024-10-13 11660. 구간 합 구하기5

---
+++ algorithm/src/10001~15000/11660.kt
@@ -0,0 +1,29 @@
+package `10001~15000`
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.StringTokenizer
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val (N, M) = br.readLine().split(" ").map { it.toInt() }
+    val numbers = Array(N+1){Array(N+1){0}}
+
+    repeat(N) { x ->
+        val st = StringTokenizer(br.readLine())
+        repeat(N) { y ->
+            numbers[x+1][y+1] = st.nextToken().toInt()+numbers[x][y+1]+numbers[x+1][y]-numbers[x][y]
+        }
+    }
+
+    repeat(M) {
+        val (x1, y1, x2, y2) = br.readLine().split(" ").map { it.toInt() }
+        bw.write("${numbers[x2][y2]-numbers[x2][y1-1]-numbers[x1-1][y2]+numbers[x1-1][y1-1]}\n")
+    }
+    bw.flush()
+    bw.close()
+    br.close()
+
+}
