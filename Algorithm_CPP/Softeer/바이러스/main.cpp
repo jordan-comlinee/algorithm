4d939699f276fc717981c1cd4cfa776dd5369ab1
Jordan <leesoyoung0112@gmail.com>
2024-10-25 14:05:24 +0900

    softeer. A+B, 바이러스

--- Algorithm_CPP/Softeer/바이러스/main.cpp
+++ Algorithm_CPP/Softeer/바이러스/main.cpp
@@ -1,11 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h>
+
+const int MAX_VALUE = 1000000007;
 
 int main(void) {
 	long long int K, P, N;
-	scanf_s("%lld %lld %lld", &K, &P, &N);
-	long long int result = K % 1000000007;
+	scanf("%lld %lld %lld", &K, &P, &N);
+	long long int result = K % MAX_VALUE;
 	for (int i = 0; i < N; i++) {
-		result *= P%1000000007;
+		result *= P%MAX_VALUE;
+		result %= MAX_VALUE;
 	}
 	printf("%lld", result);
 	return 0;
