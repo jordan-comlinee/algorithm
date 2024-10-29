448b4692676ab213308d7a7ca6fecbe023a859fa
Jordan <leesoyoung0112@gmail.com>
2024-10-29 15:58:42 +0900

    2024-10-29 Softeer Lv3. 강의실 배정

--- Algorithm_CPP/Softeer/[한양대 HCPC 2023] X marks the Spot/main.cpp
+++ Algorithm_CPP/Softeer/[한양대 HCPC 2023] X marks the Spot/main.cpp
@@ -1,30 +1,26 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <bits/stdc++.h>
+#include<stdio.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    int N;
-    cin >> N;
-    vector<pair<string, string>> puzzle(N);
-    string result;
-    result.reserve(N * 100);
-
-    for (int i = 0; i < N; i++) {
-        cin >> puzzle[i].first >> puzzle[i].second;
-    }
-
-    for (int i = 0; i < N; i++) {
-        int len = puzzle[i].first.length();
-        for (int j = 0; j < len; j++) {
-            if ((puzzle[i].first[j] == 'x' || puzzle[i].first[j] == 'X')) {
-                result += toupper(puzzle[i].second[j]);
-            }
-        }
-    }
-
-    cout << result << "\n";
-    return 0;
+	int N;
+	scanf("%d", &N);
+	vector<pair<string, string>> puzzle(N);
+	string result = "";
+	for (int i = 0; i < N; i++) {
+		string first, second;
+		scanf("%s %s", first, second);
+		puzzle[i] = make_pair(first, second);
+	}
+	for (int i = 0; i < N; i++)
+	{
+		int len = puzzle[i].first.length();
+		for (int j = 0; j < len; j++) {
+			if (puzzle[i].first[j] == 'x' || puzzle[i].first[j] == 'X') {
+				result += toupper(puzzle[i].second[j]);
+			}
+		}
+	}
+	printf("%s", result.c_str());
 }
