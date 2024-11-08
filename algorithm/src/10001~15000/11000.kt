92fc37c70766080b6755517ee43f95edec82caa0
Jordan <leesoyoung0112@gmail.com>
2024-11-08 10:12:56 +0900

    2024-11-08 11000. 강의실 배정

---
+++ algorithm/src/10001~15000/11000.kt
@@ -0,0 +1,30 @@
+package `10001~15000`
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.PriorityQueue
+import java.util.StringTokenizer
+private var cnt = 1
+fun countMaxClass(c : List<Pair<Int, Int>>) {
+    var pq = PriorityQueue<Int>()
+    for (classTime in c) {
+        if ( pq.isNotEmpty() && pq.peek() <= classTime.first) {
+            pq.poll()
+        }
+        pq.add(classTime.second)
+    }
+    cnt = pq.size
+}
+fun main(){
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val N = br.readLine().toInt()
+    val classes = mutableListOf<Pair<Int, Int>>()
+    repeat(N) {
+        val st = StringTokenizer(br.readLine())
+        val S = st.nextToken().toInt()
+        val T = st.nextToken().toInt()
+        classes.add(Pair(S, T))
+    }
+    classes.sortWith(compareBy({it.first}, {it.second}))
+    countMaxClass(classes)
+    println(cnt)
+}
