98b2af2488e438a2f3d0649670bfba1edf63e9f6
Jordan <leesoyoung0112@gmail.com>
2024-11-21 16:52:33 +0900

    2024-11-21 16928. 뱀과 사다리 게임

---
+++ Algorithm_CPP/Baek/16928/main.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int N, M, result = 0;
+int a, b;
+map<int, int> ladder, snake;
+int board[101] = { 0 };
+#define LADDER 1
+#define SNAKE 2
+
+void bfs() {
+	queue<int> visit;
+	int visited[101] = { 0 };
+	visit.push(1);
+	while (!visit.empty()) {
+		int current = visit.front();
+		visit.pop();
+
+		if (current == 100){
+			result = visited[current];
+			return;
+		}
+
+		for (int i = 1; i <= 6; i++) {
+			int next = current + i;
+			if (next > 100) continue;
+
+			int arrive = current + i;
+			if (board[next] == LADDER) {
+				arrive = ladder[next];
+			}
+			else if (board[next] == SNAKE) {
+				arrive = snake[next];
+			}
+			if (visited[arrive] == 0) {
+				visited[arrive] = visited[current] + 1;
+				visit.push(arrive);
+			}
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> N >> M;
+	for (int i = 0; i < N; i++){
+		cin >> a >> b;
+		ladder[a] = b;
+		board[a] = LADDER;
+	}
+	for (int i = 0; i < M; i++){
+		cin >> a >> b;
+		snake[a] = b;
+		board[a] = SNAKE;
+	}
+	bfs();
+	cout << result << endl;
+}
