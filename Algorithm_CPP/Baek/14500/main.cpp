cc5f8e3bcf0a63667c3f3dcd4dc27bfa78024c6b
Jordan <leesoyoung0112@gmail.com>
2024-11-24 16:31:07 +0900

    2024-11-24 14500. 테트로미노

--- Algorithm_CPP/Baek/14500/main.cpp
+++ Algorithm_CPP/Baek/14500/main.cpp
@@ -1,13 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, M, result;
+int N, M, result, maxValue = 0;
 int board[501][501] = { 0 };
+bool visited[501][501] = { false };
 int dx[4] = { 1, -1, 0, 0 };
 int dy[4] = { 0, 0, -1, 1 };
 
-void findMax(int n, int m) {
+void dfs(int n, int m , int depth, int hap) {
+	if (hap + (3 - depth) * maxValue <= result) return;
+	if (depth == 3) {
+		result = max(result, hap);
+	}
+	for (int i = 0; i < 4; i++) {
+		int nx = n + dx[i];
+		int ny = m + dy[i];
+		if ( 0 <= nx && nx < N && 0 <= ny && ny < M && !visited[nx][ny]){
+			visited[nx][ny] = true;
+			dfs(nx, ny, depth + 1, hap+board[nx][ny]);
+			visited[nx][ny] = false;
+		}
+	}
+}
 
+void checkOtherShape(int n, int m) {
+	int shapes[4][4][2] = {
+		{{0, 0}, {0, 1}, {0, 2}, {1, 1}},
+		{{0, 0}, {0, 1}, {0, 2}, {-1, 1}},
+		{{0, 0}, {1, 0}, {2, 0}, {1, 1}},
+		{{0, 0}, {1, 0}, {2, 0}, {1, -1}}
+	};
+	for (int i = 0; i < 4; i++) {
+		int sum = 0;
+		bool valid = true;
+		for (int j = 0; j < 4; j++) {
+			int nx = n + shapes[i][j][0];
+			int ny = m + shapes[i][j][1];
+			if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
+				valid = false;
+				break;
+			}
+			sum += board[nx][ny];
+		}
+		if (valid){
+			result = max(result, sum);
+		}
+	}
 }
 
 int main() {
@@ -18,11 +56,16 @@ int main() {
 	for (int n = 0; n < N; n++) {
 		for (int m = 0; m < M; m++) {
 			cin >> board[n][m];
+			maxValue = max(maxValue, board[n][m]);
 		}
 	}
 	for (int n = 0; n < N; n++) {
 		for (int m = 0; m < M; m++) {
-			findMax(n, m);
+			visited[n][m] = true;
+			dfs(n, m, 0, board[n][m]);
+			visited[n][m] = false;
+			checkOtherShape(n, m);
 		}
 	}
+	cout << result << endl;
 }
