478dfda41d2465dd1b3e9c0a4c539945dbfa2ff6
Jordan <leesoyoung0112@gmail.com>
2024-11-26 13:06:17 +0900

    2024-11-26 1967. 트리의 지름

---
+++ Algorithm_CPP/Baek/1967/main.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int N, result= 0, farthestNode = 0;
+map<int, vector<pair<int, int>>> nodes;
+
+bool comp(pair<int, int> a, pair<int, int> b) {
+	return a.first > b.first;
+}
+
+void dfs(int start, int length, vector<bool>& visited) {
+	visited[start] = true;
+	if (length > result){
+		result = length;
+		farthestNode = start;
+	}
+	for (auto &neighbor : nodes[start]) {
+		int next = neighbor.first;
+		int weight = neighbor.second;
+		if (!visited[next]) {
+			dfs(next, length + weight, visited);
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> N;
+
+	for (int n = 0; n < N-1; n++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		nodes[a].push_back(make_pair(b, c));
+		nodes[b].push_back(make_pair(a, c));
+	}
+	vector<bool> visited(N + 1, false);
+	dfs(1, 0, visited);
+	fill(visited.begin(), visited.end(), false);
+	result = 0;
+	dfs(farthestNode, 0, visited);
+	cout << result << endl;
+
+}
