1650e87ccdd6e143a87e21c362e55bf99c151ba1
Jordan <leesoyoung0112@gmail.com>
2024-12-04 17:31:03 +0900

    2024-12-04 구명보트

---
+++ python/programmersLV2/퍼즐 게임 챌린지.py
@@ -0,0 +1,13 @@
+import sys
+
+def isComplicated(time_prev, time_cur, diff, level) :
+    return (time_cur + time_prev) * (diff - level) + time_cur
+
+def calculateTotalTime(diffs, times, limit, minLevel):
+
+
+def solution(diffs, times, limit):
+    answer = 0
+    high = max(int)
+    print(high)
+    return answer
