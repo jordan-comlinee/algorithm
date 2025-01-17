d6bbeff57dd13e8affb14aaff5ea846f1f9e045c
Jordan <leesoyoung0112@gmail.com>
2025-01-17 17:31:14 +0900

    2025-01-17 practice : recursive, Double Linked List, Queue

--- Algorithm_CPP/Baek/1753/main.cpp
+++ Algorithm_CPP/Baek/1753/main.cpp
@@ -8,10 +8,13 @@ int Dist[MAX];
 vector<pair<int, int>> Vertex[MAX];
 
 void solution() {
+	// PQ는 왜 만드는거지?
 	priority_queue<pair<int, int>> PQ;
 	PQ.push(make_pair(0, K));
+	// 자기 자신의 거리는 0으로 설정
 	Dist[K] = 0;
 	while (!PQ.empty())	{
+		// 가는 데 드는 비용 - 첫번째
 		int Cost = -PQ.top().first;
 		int Cur = PQ.top().second;
 		PQ.pop();
