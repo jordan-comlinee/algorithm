a2b372f69d84b515bde5523e4de1edc1ee5e3883
Jordan <leesoyoung0112@gmail.com>
2025-01-31 10:47:55 +0900

    2025-01-31 [DP][Kotlin] 9252. LCS 2
    처음에는 이중 배열에 String을 저장하는 구조로 진행했으나, String을 수정하고, 추가하는 과정에서 메모리 초과가 발생했다.
    9251의 알고리즘을 응용하여, 위앞뒤부분과 개수를 비교하는 방식으로 LCS 결과를 유추할 수 있다.

---
+++ algorithm/src/5001~10000/9252.kt
@@ -0,0 +1,66 @@
+package `5001~10000`
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+// 9251의 코드에서 역으로 LCS를 구해낸다.
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val N = br.readLine()
+    val M = br.readLine()
+    var result = StringBuilder()
+    val NM = Array(N.length+1){Array(M.length+1){0}}
+    for(n in 1..N.length) {
+        for (m in 1..M.length) {
+            if (N[n-1] == M[m-1]) {
+                NM[n][m] = NM[n-1][m-1]+1
+            }
+            else {
+                NM[n][m] = maxOf(NM[n-1][m], NM[n][m-1])
+            }
+        }
+    }
+    bw.write("${NM[N.length][M.length]}\n")
+
+    var n = N.length
+    var m = M.length
+    while (result.length < NM[N.length][M.length]) {
+        if (NM[n][m] == NM[n-1][m]) n--
+        else if (NM[n][m] == NM[n][m-1]) m--
+        else {
+            result.insert(0, N[n-1])
+            n--
+            m--
+        }
+    }
+    bw.write("${result}")
+    bw.flush()
+    bw.close()
+}
+// 메모리 초과 - String을 계속 수정하는 과정에서 메모리 초과가 발생한 듯 하다.
+/*
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val N = br.readLine()
+    val M = br.readLine()
+    val NM = Array(N.length+1){Array<String>(M.length+1){""}}
+    for(n in 1..N.length) {
+        for (m in 1..M.length) {
+            if (N[n-1] == M[m-1]) {
+                NM[n][m] = NM[n-1][m-1]+N[n-1]
+            }
+            else {
+                if(NM[n-1][m].length > NM[n][m-1].length)
+                    NM[n][m] = NM[n-1][m]
+                else
+                    NM[n][m] = NM[n][m-1]
+            }
+        }
+    }
+    print("${NM[N.length][M.length].length}\n${NM[N.length][M.length]}")
+
+}
+*/
