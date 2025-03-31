2e3e30bba2e06d1cb047ff4a4afee9c821065067
Jordan <leesoyoung0112@gmail.com>
2025-03-31 16:52:41 +0900

    2025-03-31 [Greedy][C++] 1946. 신입 사원
    Greedy 문제.
    programmers의 인사 고과 문제와 비슷한 듯 하다.
    분제를 푸는 연습이 필요할 것 같다.
    
    풀이 시간 : 1H~

--- Algorithm_CPP/Baek/1946/main.cpp
+++ Algorithm_CPP/Baek/1946/main.cpp
@@ -3,8 +3,7 @@ using namespace std;
 
 // 테스트 케이스의 개수, 지원자의 숫자
 int T, N, a, b, result;
-vector<pair<int, int>> score;
-vector<pair<int, int>> interview;
+vector<int> score;
 
 
 void inputData();
@@ -21,35 +20,24 @@ void inputData() {
 	cin >> T;
 	for (int t = 0; t < T; ++t) {
 		cin >> N;
-		score.resize(N);
-		interview.resize(N);
+		score.resize(N+1);
+		//interview.resize(N);
 		for (int n = 0; n < N; ++n) {
 			cin >> a >> b;
-			score[n] = make_pair(a, b);
-			interview[n] = make_pair(b, a);
+			score[a] = b;
 		}
-		sort(score.begin(), score.end());
-		sort(interview.begin(), interview.end());
 		solution();
 		cout << result << endl;
 	}
 	return;
 }
 void solution() {
-	set<pair<int, int>> passed;
-	passed.insert(score[0]);
-	passed.insert(make_pair(interview[0].second, interview[0].first));
-	pair<int, int> standard = make_pair(interview[0].second, score[0].second);
-	//cout << endl;
-	//cout << standard.first << " " << standard.second << endl;
-	for (auto sc : score) {
-		if (standard.first < sc.first || standard.second < sc.second) continue;
-		cout << sc.first << " " << sc.second << endl;
-		passed.insert(sc);
+	result = 0;
+	int highest = INT32_MAX;
+
+	for (int i = 1; i <= N; ++i) {
+		if (highest > score[i]) result++;
+		highest = min(highest, score[i]);
 	}
-	cout << endl;
-	
-	result = passed.size();
-	//cout << passed.size() << endl;
 	return;
 }
